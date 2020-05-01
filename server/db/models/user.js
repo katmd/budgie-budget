f8f1b5e91c23f27474c9e484ad063a56f927c373
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2020-04-30 21:15:00 -0400

    fix: added validation to user email

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,7 +6,11 @@ const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true
+    }
   },
   password: {
     type: Sequelize.STRING,
